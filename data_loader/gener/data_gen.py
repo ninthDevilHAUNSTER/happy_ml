f211f8d3bbc8acc074f69775fbd50582bb2d7f49
ninthDevilHAUNSTER <luotianhan@126.com>
2018-12-27 23:03:53 +0800

    first commit

--- data_loader/gener/data_gen.py
+++ data_loader/gener/data_gen.py
@@ -54,6 +54,29 @@ def draw_and_renew_ellipse_data():
     draw_plot(long_list)
     renew_data(long_list, file_name='ellipse_data')
 
+def draw_and_renew_ellipse_data2():
+    long_list1 = []
+    long_list2 = []
+    long_list3 = []
+    long_list4 = []
+    # x, y = maximum_likelihood_e()
+    # long_list.append([x, y, 'r', 'max_liklyhood'])
+    for u in range(1, 5):
+        x, y = ellipse(4, 0, u * 0.1, 2.5)
+        long_list1.append([x, y, [1.0] * x.__len__(), 'b', 'ellipse x+ l'])
+        x, y = ellipse(0, 4, 2.5, u * 0.1)
+        long_list2.append([x, y, [2.0] * x.__len__(), 'r', 'ellipse y+ -'])
+        x, y = ellipse(-4, 0, u * 0.1, 2.5)
+        long_list3.append([x, y, [3.0] * x.__len__(), 'g', 'ellipse x- l'])
+        x, y = ellipse(0, -4, 2.5, u * 0.1)
+        long_list4.append([x, y, [4.0] * x.__len__(), 'y', 'ellipse y- -'])
+    # print(long_list[0][1])
+    # draw_plot(long_list)
+    renew_data(long_list1, file_name='ellipse_data_1')
+    renew_data(long_list2, file_name='ellipse_data_2')
+    renew_data(long_list3, file_name='ellipse_data_3')
+    renew_data(long_list4, file_name='ellipse_data_4')
+
 
 def renew_data(long_list, file_name):
     if os.path.exists(file_name.split('.')[0] + '.txt'):
@@ -69,4 +92,4 @@ def renew_data(long_list, file_name):
 
 
 if __name__ == '__main__':
-    draw_and_renew_ellipse_data()
+    draw_and_renew_ellipse_data2()
--- super_SVM/OVO SVMs/OVO_SVMs.py
+++ super_SVM/OVO SVMs/OVO_SVMs.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.model_selection import train_test_split
 from super_SVM.base.smoFull_within_K import calcWs, smoP
+import pickle
 
 
 def predict(ws, b, data):
@@ -18,16 +19,17 @@ def predict_value(ws, b, data):
 
 
 def loading_train_data():
-    result_mat = pd.read_csv('../..\data_loader\gener\ellipse_data.txt',
+    result_mat = pd.read_csv('../../data_loader/gener/ellipse_data.txt',
                              names=['x', 'y', 'label'], sep=',')
     class_label = result_mat.pop('label')
     return np.array(result_mat.values), np.array(class_label.values)
 
 
 def ovo_svm_main(result_mat=np.array([]), class_label=np.array([]),
-                 positive_label='1'):
-    b, alphas = smoP(result_mat, class_label, C=10, toler=0.001, maxIter=200, kTup=('lin', 1.3))
+                 positive_label=1):
+    b, alphas = smoP(result_mat, class_label, C=200, toler=0.001, maxIter=200, kTup=('lin', 1.3))
     ws = calcWs(alphas, result_mat, class_label)
+    pickle.dump([b, alphas, ws], open('./resultdata_label{}.txt'.format(positive_label), 'wb'))
     return b, ws
 
 
@@ -43,11 +45,13 @@ def main():
                 two_part_class_label[j] = 1
             else:
                 two_part_class_label[j] = -1
-            b, ws = ovo_svm_main(X_train, two_part_class_label)
-            super_variables.append([b, ws])
+        b, ws = ovo_svm_main(X_train, two_part_class_label, positive_label=int(i))
+        super_variables.append([b, ws])
         print("[*] one part done")
-    print(super_variables)
+    pickle.dump(super_variables, open('./resultData', 'wb'))
+
+    # print(super_variables)
 
 
 if __name__ == '__main__':
-    main()    # X_train, X_test, y_train, y_test = train_test_split(result_mat, class_label, test_size=0.33)
+    main()  # X_train, X_test, y_train, y_test = train_test_split(result_mat, class_label, test_size=0.33)
